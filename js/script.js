c2bb540333921c249cc26aea8de4d09bad0ff3e5
Tyler Cookson <tcookson0805@gmail.com>
2016-06-25 00:04:59 -0700

    implemented function to randomly select question

--- js/script.js
+++ js/script.js
@@ -1,27 +1,27 @@
 $(document).ready(function(){
   
-  var questions = {
-    'one' : {
-              'question' : 'Who was the 1st overall pick of the 1982 NBA Draft?',
-              'answers' : [{'Michael Jordan' : false}, {'Patrick Ewing' : false}, {'Ralph Sampson' : false}, {'James Worthy' : true}]
-            },
-    'two' : {
-              'question' : 'Who was the last NBA Rookie of the Year to NOT be drafted in the 1st Round of the NBA Draft?',
-              'answers' : [{'Willis Reed, 1964' : true}, {'Earl Monroe, 1967': false}, {'Adrian Dantley, 1976': false}, {'Sidney Wicks, 1971' : false}]
-    },
-    'three' : {
-              'question' : 'Since 1990, what team that received the 1st overall pick had the best previous season record?',
-              'answers' : [{'Orlando, 1993)' : true}, {'San Antonio, 1997': false}, {'Chicago, 2008': false}, {'Cleveland, 2014' : false}]
-    },
-    'four' : {
-              'question' : 'Since 1985, what team has received the most number 1 picks?',
-              'answers' : [{'Cleveland' : true}, {'L.A. Clippers': false}, {'Orlando': false}, {'Philadelphia' : false}]
-    },
-    'five' : {
-              'question' : 'In 1985, how many teams were in the draft lottery?',
-              'answers' : [{'Seven' : true}, {'Ten': false}, {'Nine': false}, {'Twelve' : false}]
-    }
-  }
+  var questions = [{
+                    'question' : 'Who was the 1st overall pick of the 1982 NBA Draft?',
+                    'answers' : [{'Michael Jordan' : false}, {'Patrick Ewing' : false}, {'Ralph Sampson' : false}, {'James Worthy' : true}]
+                  },
+                  {
+                    'question' : 'Who was the last NBA Rookie of the Year to NOT be drafted in the 1st Round of the NBA Draft?',
+                    'answers' : [{'Willis Reed, 1964' : true}, {'Earl Monroe, 1967': false}, {'Adrian Dantley, 1976': false}, {'Sidney Wicks, 1971' : false}]
+                  },
+                  {
+                    'question' : 'Since 1990, what team that received the 1st overall pick had the best previous season record?',
+                    'answers' : [{'Orlando, 1993)' : true}, {'San Antonio, 1997': false}, {'Chicago, 2008': false}, {'Cleveland, 2014' : false}]
+                  },
+                  {
+                    'question' : 'Since 1985, what team has received the most number 1 picks?',
+                    'answers' : [{'Cleveland' : true}, {'L.A. Clippers': false}, {'Orlando': false}, {'Philadelphia' : false}]
+                  },
+                  {
+                    'question' : 'In 1985, how many teams were in the draft lottery?',
+                    'answers' : [{'Seven' : true}, {'Ten': false}, {'Nine': false}, {'Twelve' : false}]
+                  }]
+
+  var questionsUsed = {};
 
   var count = 9
 
@@ -36,6 +36,20 @@ $(document).ready(function(){
   var triggerCountDown = function(){
     setTimeout(countdown, 1000)
   }
+  
+  var randomNumber = function(x){
+    var result = Math.floor(Math.random() * x.length)
+    return result
+  }
+  
+  var getQuestion = function(list){
+    var number = randomNumber(list);
+    
+    if(list.length > 0){
+      $('.question').text(list[number].question)
+    }
+  }
+  
 
   $('body').on('click', '.start', function(){
     
@@ -46,7 +60,8 @@ $(document).ready(function(){
     $('.answer').hover(function(){
       $(this).toggleClass('highlight');
     });
-    $('.question').text(questions.one.question);
+    
+    getQuestion(questions);
     
     triggerCountDown();
     
