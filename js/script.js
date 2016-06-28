535b56a301b00af0e7b452313bbc6964e0ffe7ba
Tyler Cookson <tcookson0805@gmail.com>
2016-06-28 11:04:59 -0700

    basic functionality of website is complete

--- js/script.js
+++ js/script.js
@@ -19,13 +19,40 @@ $(document).ready(function(){
                   {
                     'question' : 'In 1985, how many teams were in the draft lottery?',
                     'answers' : [['Seven', true], ['Ten', false], ['Nine', false], ['Twelve', false]]
-                  }]
+                  },
+                  
+                  
+                  
+                  {
+                    'question' : 'What year was the first NBA Draft?',
+                    'answers' : [['1947', true], ['1951', false], ['1943', false], ['1958', false]]
+                  },
+                  {
+                    'question' : 'Who was the very first NBA Draft pick?',
+                    'answers' : [['Clifton McNeeley', true], ['George Mikan', false], ['Chuck Share', false], ['Hot Rod Hundley', false]]
+                  },                 
+                  {
+                    'question' : 'Who was the first player drafted 1st overall straight out of High School?',
+                    'answers' : [['Kwame Brown', true], ['Lebron James', false], ['Moses Malone', false], ['Cazzie Russell', false]]
+                  },
+                  {
+                    'question' : 'Who was the 4th overall pick of the 1957 NBA Draft?',
+                    'answers' : [['Win Wilfong', true], ['Sam Jones', false], ['Jim Palmer', false], ['Charlie Tyra', false]]
+                  },
+                  {
+                    'question' : 'Who were the first college teammates to be drafted 1st overall in successive years?',
+                    'answers' : [['Dick Ricketts and Si Green', true], ['Kareem Abdul-Jabbar and Elvin Hayes', false], ['Oscar Robertson and Bob Wiesenhahn', false], ['Scott May and Kent Benson', false]]
+                  }];
 
   var questionsUsed = [];
+  
+   
   var currentQuestion = {};
   var correctAnswer = ''
+  var questionNumber = 0
+  
+  
   var count = 24;
-  var questionNumber = 0;
   var timer;
   var stop = false;
   
@@ -34,17 +61,52 @@ $(document).ready(function(){
 
 
 
+//// FUNCTIONS //////
+
+
+
+  var shuffle = function(array){
+    var result = array;
+    var temp;
+    
+    for(var i = 0; i < array.length; i++){
+      var index = Math.floor(Math.random() * array.length);
+      temp = array[i]  
+      array[i] = array[index];
+      array[index] = temp
+    }
+    return result;
+  }
+
+
   var countdown = function(){
     if(count > 0){
       count--
       $('#shot_clock_countdown').text(count)
+    }else {
+      
+      //this resets the count back to 24
+      
+      if(questions.length === 0){    
+        resetCountDown();
+        $('.final-score').text(currentScore)
+        $('.game-over').show();
+        $('.end-game').show();
+        $('.answer-box').hide();
+        $('.submit').hide();
+        $('.question').hide();
+        $('.question-tracker-text').hide();
+      }else{
+        resetCountDown()
+        triggerCountDown();
+        getQuestion(questions);  
+      } 
     }
   }
 
   var triggerCountDown = function(){
     $('#shot_clock_countdown').text(24);
     clearInterval(timer);
-    console.log(questions.length)
     if(!stop){
       if(questions.length > 0){
         timer = setInterval(countdown, 1000);
@@ -67,27 +129,32 @@ $(document).ready(function(){
     return result
   }
   
+  var newQuestionNumber = function(){
+    questionNumber = questionNumber + 1;
+    $('.question-number').text(questionNumber);
+  }
+  
   var getQuestion = function(list){
     var number = randomNumber(list);
     if(list.length > 0){
       $('.question').text(list[number].question);
       currentQuestion = list[number];
       var used = list.splice(number, 1);
-      questionsUsed.push(used);
+      questionsUsed.push(used[0]);
     }
-    questionNumber++
-    $('.question-number').text(questionNumber)
+    newQuestionNumber();
   }
-
+  
   
   var getAnswer = function(obj){
-    var q = obj['answers'];
+    var answerList = obj['answers'];
+    var shuffledAnswerList = shuffle(answerList)
     $('.answer-options').children().remove();
-    for(var i = 0; i < q.length; i++){
-      if(q[i][1]){
-        correctAnswer = q[i][0]
+    for(var i = 0; i < shuffledAnswerList.length; i++){
+      if(shuffledAnswerList[i][1]){
+        correctAnswer = shuffledAnswerList[i][0]
       }
-      $('.answer-options').append('<li class="answer shadow">' + q[i][0] + '</li>')
+      $('.answer-options').append('<li class="answer shadow">' + shuffledAnswerList[i][0] + '</li>')
     };
   }
   
@@ -106,29 +173,33 @@ $(document).ready(function(){
 
   var resetQuestions = function(){
     stop = false;
+    questionNumber = 0;
     for(var i = 0; i < questionsUsed.length; i++){
       questions.push(questionsUsed[i]);
     }
   }
   
-  var checkEndGame = function(){
-    if(!questions.length){
-      resetCountDown();
-    }
-  }
-  
+
+//// INITIAL JQUERY ////
 
 
   $('.top-score-number').text(topScore);
   $('.current-score-number').text(currentScore)
   $('.total-questions').text(questions.length)
 
-  $('body').on('click', '.start', function(){
-    
+
+//// STARTING THE GAME ////
+
+  $('body').on('click', '.start', function(){ 
+
     $('.start').hide();
-    $('.question-tracker-text').show();
+    $('.instructions').hide();
+
+    $('.question-tracker-text').show();    
+    $('.question').show();
     $('.answer-box').show();
     $('.submit').show();
+    
     $('body').on('click', '.answer', function(){
       $(this).addClass('highlight');
       $(this).siblings().removeClass('highlight')
@@ -137,43 +208,58 @@ $(document).ready(function(){
     triggerCountDown();    
     getQuestion(questions);
     getAnswer(currentQuestion);
+    
+  });
+
+//// SUBMISSION OF ANSWER ////
+
+  $('body').on('click', '.submit', function(){
+    var answer = $('.highlight').text()
 
     
-    console.log(currentQuestion)
-    
-    $('body').on('click', '.submit', function(){
-      var answer = $('.highlight').text()
-      
-      
-      checkEndGame();
+    if(questions.length === 0){
+      checkAnswer(answer, correctAnswer);      
+      resetCountDown();
+      $('.final-score').text(currentScore)
+      $('.game-over').show();
+      $('.end-game').show();
+      $('.answer-box').hide();
+      $('.submit').hide();
+      $('.question').hide();
+      $('.question-tracker-text').hide();
+    }else{
       checkAnswer(answer, correctAnswer);      
       resetCountDown();
       getQuestion(questions);
       getAnswer(currentQuestion);
-      triggerCountDown(); 
-      
-    });
-   
+      triggerCountDown();
+    }
+  });
+
+
+////  NEW GAME  ////
+
+  $('body').on('click', '#new_game', function(){
+    
+    clearInterval(timer);      
+    questionNumber = 0;
+    resetQuestions();
+    console.log(questions)
+    console.log(questionNumber)
     
 
+    $('.game-over').hide();
+    $('.end-game').hide();
+    $('.question').hide();
+    $('.instructions').show();
+    $('.start').show();      
+    $('#shot_clock_countdown').text(24)
 
-
-
-    $('body').on('click', '#new_game', function(){
-      clearInterval(timer);
-      $('#shot_clock_countdown').text(24)
-      resetQuestions();
-      $('.current-score-number').text('');
-      $('.start').show();
-      $('.question-tracker-text').hide();
-      $('.answer-box').hide();
-      $('.submit').hide();
-      $('.question').text('Welcome to the NBA Draft Quiz. To begin, simply click the Start button above.')
-    })
-
-
-  })
-  
+    $('.current-score-number').text('');
+    $('.question-tracker-text').hide();
+    $('.answer-box').hide();
+    $('.submit').hide();
+  });
   
 });
 
